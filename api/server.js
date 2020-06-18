19649fd203bc264f2da439e064c155e8998345ac
Michael Perez <mperez6473@gmail.com>
2020-06-18 11:49:33 -0500

    Added a post hobbits

--- api/server.js
+++ api/server.js
@@ -1,23 +1,34 @@
 const express = require("express");
 
 const Hobbits = require("../hobbits/hobbitsModel.js");
+const { json } = require("express");
 
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+    res.status(200).json({ api: "up" });
 });
 
 server.get("/hobbits", (req, res) => {
-  Hobbits.getAll()
-    .then(hobbits => {
-      res.status(200).json(hobbits);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    Hobbits.getAll()
+        .then((hobbits) => {
+            res.status(200).json(hobbits);
+        })
+        .catch((error) => {
+            res.status(500).json(error);
+        });
+});
+
+server.post("/hobbits", (req, res) => {
+    Hobbits.insert(req.body)
+        .then((saved) => {
+            res.status(201).json(saved);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
 });
 
 module.exports = server;
